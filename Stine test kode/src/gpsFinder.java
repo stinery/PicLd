033a4f9000e3d8fd567c551d4f15d57b69bea923
Lukas-Stjernen <lostjern@stud.ntnu.no>
2020-03-09 11:14:58 +0100

    gpsfinder ala stjernen

---
+++ Stine test kode/src/gpsFinder.java
@@ -0,0 +1,47 @@
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.lang.GeoLocation;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+import com.drew.metadata.exif.GpsDirectory;
+
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+//kokt by lukas
+
+public class gpsFinder {
+    public static <gpsDirectory> void main(String[] args) throws ImageProcessingException, IOException {
+        File bilde = new File("GOPR1206.JPG");
+
+        Metadata meta = ImageMetadataReader.readMetadata(bilde);
+
+        for (Directory directory : meta.getDirectories()) {
+            for (Tag tag : directory.getTags()) {
+                System.out.println(tag);
+            }
+        }
+
+        Directory dir = meta.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+
+        System.out.println("looking for geodata:");
+        Collection<GpsDirectory> gpsDirectories = meta.getDirectoriesOfType(GpsDirectory.class);
+        String output="gps location not found";
+        for (GpsDirectory gpsDirectory : gpsDirectories) {
+            // Try to read out the location, making sure it's non-zero
+            GeoLocation geoLocation = gpsDirectory.getGeoLocation();
+            if (geoLocation != null && !geoLocation.isZero()) {
+
+                output="gps location found: ";
+                System.out.println(output+geoLocation);
+
+            }
+        }
+        System.out.println(output);
+    }
+}
