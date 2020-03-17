30017f09612b8f26a11bda2e175af932f618c0d2
chloekh <chloek2707@gmail.com>
2020-03-17 17:26:28 +0100

    Test

--- Stine test kode/src/testing.java
+++ Stine test kode/src/testing.java
@@ -1,4 +1,4 @@
-public class testing {
+public class testing { //Testing
     public static void main(String[] args) {
         InterestingMetadata p = new InterestingMetadata("C:/Users/olelo/OneDrive/Bilder/trumpmajor.jpg");
         int i = 0;
