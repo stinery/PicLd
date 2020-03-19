4aeb993e5070dfbaefa6344156833fa0b0682080
Ole Lokken <ole.lokken1@gmail.com>
2020-03-19 13:12:34 +0100

    Klassen Album med sorteringsmetoder

--- Stine test kode/src/albumtest.java
+++ Stine test kode/src/albumtest.java
@@ -5,5 +5,6 @@ public class albumtest {
         a.addAlbum("Johans");
         a.addPhoto("Snekker", "C:/Users/olelo/OneDrive/Bilder/Kamerabilder/WIN_20200308_21_00_11_Pro.jpg");
         int i = 0;
+        System.out.println("hallo");
     }
 }
